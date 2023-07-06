d42b4f8df01e5bc33e5000f4328242f14f5156ac
deezyfg <mensahpeter421@gmail.com>
2023-07-06 11:43:29 +0000

    Updated Activity

--- app/src/main/java/com/example/reciper/Activity2.java
+++ app/src/main/java/com/example/reciper/Activity2.java
@@ -2,13 +2,39 @@ package com.example.reciper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
+    EditText password;
+    Button loginButton;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        password = findViewById(R.id.Password);
+        loginButton = findViewById(R.id.login_button);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (password.getText().toString().equals("1234")) {
+                    Toast.makeText(Activity2.this, "Login successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(Activity2.this, "Invalid password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
+
+
