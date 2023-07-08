fe5cf7bf78bde1c245820abc824852cf8a3ab089
IddrisTipagya <136504385+IddrisTipagya@users.noreply.github.com>
2023-07-08 18:09:15 +0000

    Update Activity5.java

--- app/src/main/java/com/example/reciper/Activity5.java
+++ app/src/main/java/com/example/reciper/Activity5.java
@@ -1,5 +1,6 @@
 package com.example.reciper;
 
+//Imported Dependencies
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
