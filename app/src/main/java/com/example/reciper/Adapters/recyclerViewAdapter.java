8f510b6d022a69a18d9548533b3874f45864a30d
nellyodai <nellyodai7@gmail.com>
2023-07-08 02:43:55 +0000

    check it out

--- app/src/main/java/com/example/reciper/Adapters/recyclerViewAdapter.java
+++ app/src/main/java/com/example/reciper/Adapters/recyclerViewAdapter.java
@@ -1,5 +1,7 @@
 package com.example.reciper.Adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,27 +11,37 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.reciper.R;
+import com.example.reciper.cheeseBurger;
 
 import java.util.ArrayList;
 
 public class recyclerViewAdapter extends RecyclerView.Adapter<recyclerViewAdapter.MyViewHolder >{
-
-    public recyclerViewAdapter(Context context, ArrayList<burgerModels>)
+    Context context;
+    ArrayList<cheeseBurger> cheeseBurgers;
+    public recyclerViewAdapter(Context context, ArrayList<cheeseBurger> cheeseBurgers){
+        this.context = context;
+        this.cheeseBurgers = cheeseBurgers;
+    }
 
     @NonNull
     @Override
     public recyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_row, parent, false);
+        return new recyclerViewAdapter.MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull recyclerViewAdapter.MyViewHolder holder, int position) {
+    holder.textView.setText(cheeseBurgers.get(position).getBurgerName());
+    holder.imageView.setImageResource(cheeseBurgers.get(position).getImage());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+
+        return cheeseBurgers.size();
     }
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
